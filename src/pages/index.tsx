1ff85dd399f2e84198a9e0d242765ae0d83fd4dd
LocaMartin <locaboyff@gmail.com>
2024-11-15 11:10:16 +0530

    initial push

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  @apply bg-gray-100 text-gray-900 min-h-screen font-sans antialiased;
+}
+
+* {
+  transition: all 0.2s ease-in-out;
+}
